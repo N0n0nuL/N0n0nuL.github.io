c426b770969cf52de74d22c0cb869272782bc795
N0n0nuL <56295099+N0n0nuL@users.noreply.github.com>
2021-05-24 17:54:32 +0200

    Update app2.js

--- js/app2.js
+++ js/app2.js
@@ -1,65 +1,56 @@
-mobiscroll.settings = {
-    lang: 'fr',
-    theme: 'material',
-    themeVariant: 'light'
-};
+const container = document.getElementById('container');
+const loading = document.querySelector('.loading');
 
-var instance,
-    fromValues = [],
-    toValues = [];
+getPost();
+getPost();
+getPost();
 
-for (var i = 0; i <= 1000; i += 20) {
-    fromValues.push({
-        display: '$' + i,
-        value: i
-    });
+window.addEventListener('scroll', () => {
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	
+	console.log( { scrollTop, scrollHeight, clientHeight });
+	
+	if(clientHeight + scrollTop >= scrollHeight - 5) {
+		// show the loading animation
+		showLoading();
+	}
+});
+
+function showLoading() {
+	loading.classList.add('show');
+	
+	// load more data
+	setTimeout(getPost, 1000)
 }
 
-toValues = fromValues.slice();
-toValues.shift();
-toValues.push({
-    display: 'over $1000'
-});
+async function getPost() {
+	const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${getRandomNr()}`);
+	const postData = await postResponse.json();
+	
+	const userResponse = await fetch('https://randomuser.me/api');
+	const userData = await userResponse.json();
+	
+	const data = { post: postData, user: userData.results[0] };
+	
+	addDataToDOM(data);
+}
 
-instance = mobiscroll.scroller('#demo', {
-    display: 'inline',
-    width: 150,
-    wheels: [
-        [{
-            circular: false,
-            data: fromValues,
-            label: 'From'
-        }, {
-            circular: false,
-            data: toValues,
-            label: 'To'
-        }]
-    ],
-    showLabel: true,
-    validate: function (event, inst) {
-        var i,
-            values = event.values,
-            disabledValues = [];
+function getRandomNr() {
+	return Math.floor(Math.random() * 100) + 1;
+}
 
-        for (i = 0; i < toValues.length; ++i) {
-            if (toValues[i].value <= values[0]) {
-                disabledValues.push(toValues[i].value);
-            }
-        }
-
-        return {
-            disabled: [
-                [], disabledValues
-            ]
-        }
-    },
-    formatValue: function (data) {
-        return '$' + data[0] + ' -  $' + data[1];
-    },
-    parseValue: function (valueText) {
-        if (valueText) {
-            return valueText.replace(/\s/gi, '').split('-');
-        }
-        return [0, 20];
-    }
-});
+function addDataToDOM(data) {
+	const postElement = document.createElement('div');
+	postElement.classList.add('blog-post');
+	postElement.innerHTML = `
+		<h2 class="title">${data.post.title}</h2>
+		<p class="text">${data.post.body}</p>
+		<div class="user-info">
+			<img src="${data.user.picture.large}" alt="${data.user.name.first}" />
+			<span>${data.user.name.first} ${data.user.name.last}</span>
+		</div>
+	`;
+	container.appendChild(postElement);
+	
+	loading.classList.remove('show');
+}
